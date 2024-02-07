4f3f8f6ee25970f4060d128008503829ccbe68c6
Florian <florian.b.wende@gmail.com>
2024-02-07 21:10:43 +0200

    dotenv

--- pygh.py
+++ pygh.py
@@ -7,10 +7,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+GH_ACCESS_TOKEN = os.getenv('GITHUB_ACCESS_TOKEN')
 
 # using an access token
-auth = Auth.Token("access_token")
+auth = Auth.Token(GH_ACCESS_TOKEN)
 
 # Public Web Github
 g = Github(auth=auth)
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    repo.edit(has_wiki=False)
+    # to see all the available attributes and methods
+    print(dir(repo))
